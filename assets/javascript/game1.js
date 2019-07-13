9cadbc25313a1b7f855da3d6de2cbb0255926041
kaur1081 <karishmawalia@gmail.com>
2019-07-13 16:44:28 -0700

    last upgrade

--- assets/javascript/game1.js
+++ assets/javascript/game1.js
@@ -1,6 +1,6 @@
 // Creates an array that lists out all of the options (a to z).
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var computerGuess = "a";
+var computerGuess = null;
 var userGuesses = [];
 var guessesLeft = 9;
 var wins = 0;
